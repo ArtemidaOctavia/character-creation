cc5a130f4f2189d90962e03783f1dcc902c43d57
ArtemidaOctavia <42804865+ArtemidaOctavia@users.noreply.github.com>
2020-06-26 23:19:06 +0300

    Updates

---
+++ precache-manifest.f9bc9efecfb36d6a977b3e5726d9e297.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba8dc8a2248c4e4aef47f73c1014c4a5",
+    "url": "/character-creation/index.html"
+  },
+  {
+    "revision": "db145571e606902021d6",
+    "url": "/character-creation/static/css/main.679cb190.chunk.css"
+  },
+  {
+    "revision": "8d5b8000f5516a841003",
+    "url": "/character-creation/static/js/2.bda0cb71.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/character-creation/static/js/2.bda0cb71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db145571e606902021d6",
+    "url": "/character-creation/static/js/main.fac853c2.chunk.js"
+  },
+  {
+    "revision": "0a81f1485d985977068d",
+    "url": "/character-creation/static/js/runtime-main.722c59d3.js"
+  }
+]);
